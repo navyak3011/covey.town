c5ce77aea591771e85ba28888ac273ef94eaaf5f
navyak <Mysore6322!!>
2021-04-13 00:02:39 -0400

    Updated APIs and models

--- backend/models/room.model.js
+++ backend/models/room.model.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
+/*const beautifyUnique = require('mongoose-beautiful-unique-validation');
+
+mongoose.plugin(beautifyUnique, {
+  defaultMessage: "duplication detected"
+});
+mongoose.set('useCreateIndex', true);*/
+
 var Schema = mongoose.Schema;
 
 var roomSchema = new Schema({
-                              roomId: String,
-                              friendlyName: String,
-                              isPublic: Boolean
-                            });
+  roomId: { type: String, unique: true },
+  friendlyName: String,
+  isPublic: Boolean,
+  password: String,
+});
 
-var room = mongoose.model('roomEntry', roomSchema);
+//roomSchema.plugin(beautifyUnique);
+var room = mongoose.model("roomEntry", roomSchema);
 module.exports = room;
